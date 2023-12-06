b4de511cf7cbf8fff52572f805c513ad55d4a315
salahect <flowchino94@gmail.com>
2023-12-06 10:06:36 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//Intancia de Express
+
+
+//Configurar Puerto
+
+//Funcion Escribir Datos
+
+//Configurar Ruta
+
+//GET
+
+//GET LIBRO ID
+
+//POST
+
+//PUT
+
+//DELETE
+
+
+//Escuchar app por puerto 3000
